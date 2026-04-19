f91f26c2d6b770000009239e02a8351f4b334668
efilonaru <naufalyasir5467@gmail.com>
2026-04-19 19:52:05 +0800

    Initial Commit

---
+++ WeatherFarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherFarm
+//
+//  Created by Naufal Muafa on 19/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherFarm/WeatherFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFarmApp.swift
+//  WeatherFarm
+//
+//  Created by Naufal Muafa on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
